b8dc20f64f316e20ee199a2b61b55634ebbe6bce
Nicolas <nicolasgfalabella@gmail.com>
2024-01-03 13:36:42 -0300

    Inicio de Proyecto - Configuracion de Variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,21 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        lemon: '#C0FD35',
+        greylight: '#3A393E',
+        greydark: '#202022',
+        rederror: '#FF0004',
+      },
+      screens: {
+        'xs': '290px',
+        'sm': '640px',  
+        'md': '768px',  
+        'lg': '1024px', 
+        'xl': '1280px',
+        '2xl': '1440px',        
+      },
     },
   },
   plugins: [],
-}
+};
