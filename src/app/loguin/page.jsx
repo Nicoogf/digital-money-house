881f588adf44857ceffd13382a174c35d63f8427
Nicolas <nicolasgfalabella@gmail.com>
2024-01-03 22:46:43 -0300

    Sprint 1 : Fix de paddings y anchos

--- src/app/loguin/page.jsx
+++ src/app/loguin/page.jsx
@@ -13,8 +13,8 @@ const page = () => {
       '>
 
         <h3 className='text-white font-semibold text-center text-xl
-                        sm:text-4xl
-                        md:text-5xl
+                        sm:text-3xl
+                        md:text-4xl
                         xl:text-3xl'> 
             ¡Hola! Ingresá tu e-mail
         </h3>
@@ -23,31 +23,31 @@ const page = () => {
                 className='text-white py-3 rounded-lg bg-greystandar box-border pl-4 outline-none
                            placeholder:pl-4 placeholder:text-gray-500 active:text-grey-dark
                            w-[80%]
-                           sm:w-[50%] sm:text-2xl
-                           md:py-6
-                           lg:w-[20%] lg:py-4 lg:text-xl
-                           xl:w-[18%] lg:py-3 lg:text-lg'/>
+                           sm:w-[50%] sm:text-xl
+                           md:py-5
+                           lg:w-[25%] lg:py-3 lg:text-xl
+                           xl:w-[18%] lg:py-3 lg:text-base'/>
 
         <a className='bg-lemon block text-center font-semibold py-3 rounded-lg cursor-pointer border-2
                       w-[80%] border-transparent hover:bg-transparent hover:text-lemon hover:border-lemon
                       transition-all duration-500
-                        sm:w-[50%] sm:text-2xl sm:py-4
-                        md:py-6
-                        lg:w-[20%] lg:py-4 lg:text-xl
-                        xl:w-[18%] lg:py-3 lg:text-lg'
+                        sm:w-[50%] sm:text-xl sm:py-4
+                        md:py-5
+                        lg:w-[25%] lg:py-3 lg:text-xl
+                        xl:w-[18%] lg:py-3 lg:text-base'
         > Confirmar </a>
 
         <a className='bg-greylight block text-center font-semibold py-3 rounded-lg cursor-pointer border-2
                         w-[80%] border-transparent hover:bg-transparent hover:text-greylight hover:border-greylight
                         transition-all duration-500
-                        sm:w-[50%] sm:text-2xl sm:py-4
-                        md:py-6
-                        lg:w-[20%] lg:py-4 lg:text-xl
-                        xl:w-[18%] lg:py-3 lg:text-lg'
+                        sm:w-[50%] sm:text-xl sm:py-4
+                        md:py-5
+                        lg:w-[25%] lg:py-3 lg:text-xl
+                        xl:w-[18%] lg:py-3 lg:text-base'
         > Crear Cuenta </a>
         
-        <p className='bg-red-600/100 text-xs text-white box-border px-2 py-1 rounded-md mt-8
-                      sm:text-md'>
+        <p className='bg-red-600/100 text-xs font-semibold text-white box-border px-4 py-2 rounded-md mt-8
+                      sm:text-text-base'>
           Usuario inexistente, vuelve a intentarlo
         </p>
 
